9dda5e7f8a9f27168814e75a9aa42d1ab9e88ba5
Ajaya Kumar <ajayakumar4741@gmail.com>
2026-02-09 21:36:47 +0530

    moeded axios

--- src/api.js
+++ src/api.js
@@ -3,8 +3,12 @@ import { jwtDecode } from 'jwt-decode'
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL
 
+
+
+
 const api = axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+     withCredentials: true
 })
 
 api.interceptors.request.use(
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from 'react-router-dom';
@@ -12,21 +12,21 @@ import InputErrors from '@/ui_components/InputErrors';
 import axios from 'axios';
 
 
-function SignupPage({updateForm,userInfo,toggleModal}) {
-  const { register, handleSubmit, formState, reset, watch } = useForm({defaultValues: userInfo ? userInfo : {}});
+function SignupPage({ updateForm, userInfo, toggleModal }) {
+  const { register, handleSubmit, formState, reset, watch } = useForm({ defaultValues: userInfo ? userInfo : {} });
   const { errors } = formState;
   const password = watch("password");
   const queryClient = useQueryClient()
   const [captchaImage, setCaptchaImage] = useState("");
   const [captchaKey, setCaptchaKey] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     loadCaptcha()
-  },[])
+  }, [])
 
   const loadCaptcha = async () => {
     try {
-      const res = await axios.get("http://localhost:8000/api/get_captcha/");
+      const res = await axios.get("http://localhost:8000/api/get_captcha/", { withCredentials: true });
       setCaptchaKey(res.data.key);
       setCaptchaImage("http://localhost:8000" + res.data.image_url);
     } catch (err) {
@@ -40,7 +40,7 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
     onSuccess: () => {
       toast.success("Profile updated successfully !!!")
       toggleModal()
-      queryClient.invalidateQueries({queryKey: ['users',userInfo?.username]})
+      queryClient.invalidateQueries({ queryKey: ['users', userInfo?.username] })
     },
     onError: (err) => {
       toast.error(err)
@@ -61,64 +61,64 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
   // });
 
   const mutation = useMutation({
-  mutationFn: registerUser,
-  onSuccess: () => {
-    toast.success("Account created successfully!");
-    reset();
-    loadCaptcha();
-  },
-  onError: (err) => {
-    const msg =
-      err?.response?.data?.captcha?.[0] ||
-      "Invalid captcha. Please try again.";
+    mutationFn: registerUser,
+    onSuccess: () => {
+      toast.success("Account created successfully!");
+      reset();
+      loadCaptcha();
+    },
+    onError: (err) => {
+      const msg =
+        err?.response?.data?.captcha?.[0] ||
+        "Invalid captcha. Please try again.";
 
-    toast.error(msg);
-    loadCaptcha();
-  },
-});
+      toast.error(msg);
+      loadCaptcha();
+    },
+  });
 
 
 
   function onSubmitData(data) {
-    if(updateForm){
+    if (updateForm) {
       const formData = new FormData()
-      formData.append('username',data.username)
-      formData.append('first_name',data.first_name)
-      formData.append('last_name',data.last_name)
-      formData.append('job_title',data.job_title)
-      formData.append('bio',data.bio)
-      if(data.profile_picture && data.profile_picture[0]){
-        if(data.profile_picture[0] != '/'){
-          formData.append('profile_picture',data.profile_picture[0])
+      formData.append('username', data.username)
+      formData.append('first_name', data.first_name)
+      formData.append('last_name', data.last_name)
+      formData.append('job_title', data.job_title)
+      formData.append('bio', data.bio)
+      if (data.profile_picture && data.profile_picture[0]) {
+        if (data.profile_picture[0] != '/') {
+          formData.append('profile_picture', data.profile_picture[0])
         }
       }
       updateProfileMutation.mutate(formData)
 
     }
-    else{
+    else {
       mutation.mutate({
         ...data,
         captcha_0: captchaKey,
         captcha_1: data.captcha_value
       });
     }
-    
+
   }
 
   return (
     <form
-      className={`${updateForm && "h-[90%] overflow-auto" }  overflow-auto md:px-16 px-8 py-6 flex flex-col mx-auto my-9 items-center gap-4 w-fit rounded-lg bg-[#FFFFFF] shadow-xl dark:text-white dark:bg-[#141624]`}
+      className={`${updateForm && "h-[90%] overflow-auto"}  overflow-auto md:px-16 px-8 py-6 flex flex-col mx-auto my-9 items-center gap-4 w-fit rounded-lg bg-[#FFFFFF] shadow-xl dark:text-white dark:bg-[#141624]`}
       onSubmit={handleSubmit(onSubmitData)}
     >
       <div className="flex flex-col gap-2 justify-center items-center mb-2">
         <h3 className="font-semibold text-2xl">{updateForm ? 'Update Profile' : 'Sign Up'}</h3>
         <p className="text-center text-sm text-gray-600 dark:text-gray-400">
-          {updateForm ? "You can tell us more" : 
-          'Create your account to get started!'}
+          {updateForm ? "You can tell us more" :
+            'Create your account to get started!'}
         </p>
       </div>
-      
-      
+
+
       <div className="flex flex-col gap-2 mb-2">
         <Label htmlFor="username" className="dark:text-[97989F]">Username</Label>
         <Input
@@ -136,7 +136,7 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
         />
         {errors?.username && <small className='text-red-700'>{errors.username.message}</small>}
       </div>
- 
+
       <div className="flex flex-col gap-2 mb-2">
         <Label htmlFor="first_name">First Name</Label>
         <Input
@@ -155,7 +155,7 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
         {errors?.first_name && <small className='text-red-700'>{errors.first_name.message}</small>}
       </div>
 
-      
+
       <div className="flex flex-col gap-2 mb-2">
         <Label htmlFor="last_name">Last Name</Label>
         <Input
@@ -174,7 +174,7 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
         {errors?.last_name && <small className='text-red-700'>{errors.last_name.message}</small>}
       </div>
 
-      
+
 
       {updateForm && <div className="flex flex-col gap-2 mb-2">
         <Label htmlFor="job_title" className="dark:text-[97989F]">
@@ -228,53 +228,58 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
           className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-full max-sm:w-[300px] max-sm:text-[14px]"
         />
 
-        
+
       </div>}
 
-      {updateForm || 
-      <div className="flex flex-col gap-2 mb-2">
-        <Label htmlFor="password">Password</Label>
-        <Input
-          type="password"
-          id="password"
-          placeholder="Enter password"
-          {...register('password', {
-            required: 'Password is required',
-            minLength: {
-              value: 8,
-              message: "Password must be at least 8 characters long"
-            }
-          })}
-          className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[300px]"
-        />
-        {errors?.password && <small className='text-red-700'>{errors.password.message}</small>}
-      </div>}
+      {updateForm ||
+        <div className="flex flex-col gap-2 mb-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            {...register('password', {
+              required: 'Password is required',
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters long"
+              }
+            })}
+            className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[300px]"
+          />
+          {errors?.password && <small className='text-red-700'>{errors.password.message}</small>}
+        </div>}
 
-      {updateForm || 
-      <div className="flex flex-col gap-2 mb-2">
-        <Label htmlFor="confirmPassword">Confirm Password</Label>
-        <Input
-          type="password"
-          id="confirmPassword"
-          placeholder="Confirm password"
-          {...register('confirmPassword', {
-            required: 'Confirm password is required',
-            minLength: {
-              value: 8,
-              message: "Password must be at least 8 characters long"
-            },
-            validate: (value) => value === password || 'Passwords do not match'
-          })}
-          className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[300px]"
-        />
-        {errors?.confirmPassword && <small className='text-red-700'>{errors.confirmPassword.message}</small>}
-      </div>}
+      {updateForm ||
+        <div className="flex flex-col gap-2 mb-2">
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm password"
+            {...register('confirmPassword', {
+              required: 'Confirm password is required',
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters long"
+              },
+              validate: (value) => value === password || 'Passwords do not match'
+            })}
+            className="border-2 border-[#141624] dark:border-[#3B3C4A] focus:outline-0 h-[40px] w-[300px]"
+          />
+          {errors?.confirmPassword && <small className='text-red-700'>{errors.confirmPassword.message}</small>}
+        </div>}
 
       {!updateForm && (
         <div className="flex flex-col gap-2 mb-2">
           <Label htmlFor="captcha">Captcha</Label>
           {captchaImage && (
-            <img src={captchaImage} alt="captcha" className="border p-2 mb-2" />
+            <img
+              src={`${captchaImage}?t=${Date.now()}`}
+              alt="captcha"
+              crossOrigin="use-credentials"
+              className="border p-2 mb-2"
+            />
           )}
           <Input
             type="text"
@@ -296,38 +301,38 @@ function SignupPage({updateForm,userInfo,toggleModal}) {
 
       {/* Submit Button */}
       <div className="w-full flex items-center justify-center flex-col my-4">
-        {updateForm ? 
-        <button
-          type="submit"
-          className="bg-[#4B6BFB] text-white w-full py-3 px-2 rounded-md flex items-center justify-center gap-2"
-        >
-          {updateProfileMutation.isPending ? (
-            <>
-              <SmallSpinner />
-              <small className='text-[16px]'>Updating profile...</small>
-            </>
-          ) : (
-            <small className='text-[16px]'>Update profile</small>
-          )}
-        </button> :
-        <button
-          type="submit"
-          className="bg-[#4B6BFB] text-white w-full py-3 px-2 rounded-md flex items-center justify-center gap-2"
-        >
-          {mutation.isPending ? (
-            <>
-              <SmallSpinner />
-              <small className='text-[16px]'>Creating user...</small>
-            </>
-          ) : (
-            <small className='text-[16px]'>Signup</small>
-          )}
-        </button>
+        {updateForm ?
+          <button
+            type="submit"
+            className="bg-[#4B6BFB] text-white w-full py-3 px-2 rounded-md flex items-center justify-center gap-2"
+          >
+            {updateProfileMutation.isPending ? (
+              <>
+                <SmallSpinner />
+                <small className='text-[16px]'>Updating profile...</small>
+              </>
+            ) : (
+              <small className='text-[16px]'>Update profile</small>
+            )}
+          </button> :
+          <button
+            type="submit"
+            className="bg-[#4B6BFB] text-white w-full py-3 px-2 rounded-md flex items-center justify-center gap-2"
+          >
+            {mutation.isPending ? (
+              <>
+                <SmallSpinner />
+                <small className='text-[16px]'>Creating user...</small>
+              </>
+            ) : (
+              <small className='text-[16px]'>Signup</small>
+            )}
+          </button>
         }
-        {updateForm || 
-        <p className="text-[14px] mt-2">
-          Already have an account? <Link to="/login" className="text-blue-600 underline">Sign In</Link>
-        </p>}
+        {updateForm ||
+          <p className="text-[14px] mt-2">
+            Already have an account? <Link to="/login" className="text-blue-600 underline">Sign In</Link>
+          </p>}
       </div>
     </form>
   );
