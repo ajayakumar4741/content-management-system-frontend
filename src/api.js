7f58fa33b7266feb6af031e1bb9d5904268cb25c
Ajaya Kumar <ajayakumar4741@gmail.com>
2026-09-07 18:03:15 +0530

    added content type

--- src/api.js
+++ src/api.js
@@ -8,7 +8,10 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 const api = axios.create({
     baseURL: BASE_URL,
-    
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
 })
 
 api.interceptors.request.use(
